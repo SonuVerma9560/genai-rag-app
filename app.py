353dbd94f5b69e1b7d4d1f2cf1821143bde0a8c8
Sonu Verma <sonuverma9560@gmail.com>
2026-03-20 12:59:51 +0530

    fix versions

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from openai import OpenAI
 
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import OpenAIEmbeddings
 
 # Load API key from Streamlit secrets
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
